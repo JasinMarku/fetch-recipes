fba0eae4185ecdf53a053957cc98c790b8b5acc3
Jasin Marku <jasinmarkuu@gmail.com>
2024-12-11 18:43:30 -0500

    Refactor: Minor UI adjustments to empty state positioning and refresh button

--- Fetch Recipes/Fetch Recipes/Features/Recipes/Views/RecipeListView.swift
+++ Fetch Recipes/Fetch Recipes/Features/Recipes/Views/RecipeListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RecipeListView: View {
     
     @StateObject private var viewModel = RecipeListViewModel()
+    @State private var isRotating = false // Tracks rotation state
     
     var body: some View {
         NavigationStack {
@@ -22,6 +23,8 @@ struct RecipeListView: View {
                                        message: "Looks like something went wrong.\nRefresh or try again soon.",
                                        image: "emptystate")
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    .offset(y: -50)
                 } else {
                     List(viewModel.recipes, id: \.uuid) { recipe in
                         RecipeCell(recipe: recipe)
@@ -32,8 +35,13 @@ struct RecipeListView: View {
                 }
             }
             .navigationTitle("Recipes")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    reloadButton
+                }
+            }
             .task {
-                await viewModel.fetchRecipes()
+                await viewModel.fetchRecipes() // remove to test empty state
             }
         }
     }
@@ -41,4 +49,29 @@ struct RecipeListView: View {
 
 #Preview {
     RecipeListView()
+}
+
+extension RecipeListView {
+    var reloadButton: some View {
+        Button {
+            Task {
+                withAnimation(.linear(duration: 1)) {
+                    isRotating = true
+                }
+                await viewModel.fetchRecipes()
+                isRotating = false
+            }
+        } label: {
+            Image(systemName: "arrow.clockwise")
+                .rotationEffect(.degrees(isRotating ? 360 : 0))
+                .animation(
+                    isRotating ?
+                        .linear(duration: 1)
+                        .repeatForever(autoreverses: false) :
+                        .default,
+                    value: isRotating
+                )
+                .tint(.primary)
+        }
+    }
 }
